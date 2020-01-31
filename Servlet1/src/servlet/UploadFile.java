af4afaafab579c07311c3b87369a8d3b87cbf506
Payal-Neeraj <53444609+Payal-Neeraj@users.noreply.github.com>
2020-01-31 21:59:25 +0530

    upload file

---
+++ Servlet1/src/servlet/UploadFile.java
@@ -0,0 +1,74 @@
+package servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.MultipartConfig;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class UploadServlet
+ */
+@WebServlet("/UploadServlet")
+@MultipartConfig
+
+
+public class UploadFile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+    private static final String UPLOAD_DIR = "uploads";
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadFile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("type/html");
+		PrintWriter ps=response.getWriter();
+		//String applicationPath = request.getServletContext().getRealPath("");
+        // constructs path of the directory to save uploaded file
+        String uploadFilePath = "D:\\uploads" + File.separator + UPLOAD_DIR;
+         
+        // creates the save directory if it does not exists
+        File fileSaveDir = new File(uploadFilePath);
+        if (!fileSaveDir.exists()) {
+            fileSaveDir.mkdirs();
+        }
+        System.out.println("Upload File Directory="+fileSaveDir.getAbsolutePath());
+        
+        String fileName = null;
+        //Get all the parts from request and write it to the file on server
+        for (Part part : request.getParts()) {
+            fileName = getFileName(part);
+            part.write(uploadFilePath + File.separator + fileName);
+        }
+ 
+       
+        ps.println("File uploaded successfully..."); 
+	}
+	
+	private String getFileName(Part part) {                            //gives the file name which was to be uploaded
+        String contentDisp = part.getHeader("content-disposition");
+        System.out.println("content-disposition header= "+contentDisp);
+        String[] tokens = contentDisp.split(";");
+        for (String token : tokens) {
+            if (token.trim().startsWith("filename")) {
+                return token.substring(token.indexOf("=") + 2, token.length()-1);
+            }
+        }
+        return "";
+    }
+}
+
